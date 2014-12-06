f1cc2f006d02c9a50c76cc76eb655803644532ed
jquag <mail@johnquagliata.com>
2014-12-06 10:42:08 -0600

    switched to node modules

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-requirejs.config({
-    baseUrl: 'lib',
-    paths: {
-        app: '../app'
-    }
-});
+(function () {
+  'use strict';
 
-requirejs(['app/main']);
+  var moment = require('moment');
+  var sprintf = require('sprintf');
+
+  var timer = document.getElementById('timer');
+  var lookAwayAt = moment().add(1, 'minutes');
+  var lookingAtScreenLoop = setInterval(function() {
+    var duration = moment.duration(lookAwayAt.diff(moment()));
+    if (duration.asMilliseconds() <= 0) {
+      clearInterval(lookingAtScreenLoop);
+      timer.textContent = '00 : 00';
+    } else {
+      timer.textContent = sprintf('%02d', duration.minutes()) + ' : ' + sprintf('%02d', duration.seconds());
+    }
+  }, 200);
+}());
